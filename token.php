a20888c68e640d070cc155826afe517f72efc61d
Teebu Philip <teebu.philip@gmail.com>
2021-02-24 15:19:15 -0500

    updated w/creds

--- token.php
+++ token.php
@@ -2,8 +2,8 @@
 header('Access-Control-Allow-Origin: ' . $_SERVER['SERVER_NAME']);
 
 // Replace with your own subscription key and service region (e.g., "westus").
-$subscriptionKey = 'YourSubscriptionKey';
-$region = 'YourServiceRegion';
+$subscriptionKey = 'ed20b21c-1508-4066-b391-3e1afae0527f';
+$region = 'centralus';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://' . $region . '.api.cognitive.microsoft.com/sts/v1.0/issueToken');
